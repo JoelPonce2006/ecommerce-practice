bd7968899dd06cab9715f724c975f72a0cfb6a72
Jandry Ponce <jandryponceloor@outlook.com>
2025-06-25 20:31:56 -0500

    Agregar Modal Succesfull Order

--- main.js
+++ main.js
@@ -4,12 +4,29 @@ var backdrop = document.getElementById("backdrop");
 
 var menu = document.getElementById("menu");
 
+var toPay = document.getElementById("to-pay");
+
+var payOrder = document.getElementById("pay-order");
+
 menuHamburguer.addEventListener("click", () => {
   if (menu.classList.contains("header-menu-open")) {
     menu.classList.remove("header-menu-open");
     backdrop.classList.remove("backdrop-open");
   } else {
     menu.classList.add("header-menu-open");
+    backdrop.classList.add("backdrop-open");
+  }
+});
+
+toPay.addEventListener("click", () => {
+  var isOpen = payOrder.classList.contains(
+    "modal-succesfull-order-container-open"
+  );
+  if (isOpen) {
+    payOrder.classList.remove("modal-succesfull-order-container-open");
+    backdrop.classList.remove("backdrop-open");
+  } else {
+    payOrder.classList.add("modal-succesfull-order-container-open");
     backdrop.classList.add("backdrop-open");
   }
 });
